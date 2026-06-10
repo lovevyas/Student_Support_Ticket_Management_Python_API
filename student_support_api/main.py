bf78de97f6ed6edb774a026c5373ee53994f6ab5
lovevyas <love1252.be21@chitkarauniversity.edu.in>
2026-06-10 20:02:27 +0530

    authentication completed

--- student_support_api/main.py
+++ student_support_api/main.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI
 from schemas.ticket import TicketCreate
 from schemas.response import APIResponse
 from schemas.ticket import TicketBase
-
+from schemas.user import UserLogin
 app = FastAPI(title="Student Support Ticket API")
 tickets_db = []
+users_db = [
+    {
+        "id": 1,
+        "username": "student",
+        "password": "student123",
+        "role": "student"
+    },
+    {
+        "id": 2,
+        "username": "support",
+        "password": "support123",
+        "role": "support"
+    }
+]
 
 @app.get("/")
 def home():
@@ -34,5 +48,29 @@ def get_ticket():
         message = "All tickets are here",
         data = tickets_db
     )
+       
+
+
+@app.post("/login", response_model = APIResponse) 
+def userlogin(user: UserLogin):
+    for user_detail in users_db:
+        if(user_detail["username"] == user.username 
+           and user_detail["password"] == user.password):
+                return APIResponse(
+                    success=True,
+                    message="Login successful",
+                    data= {
+                        "id": user_detail["id"],
+                        "username": user_detail["username"],
+                        "role": user_detail["role"]
+                        
+                    }
+                )
+    return APIResponse(
+        success=False,
+        message="Invalid credentials",
+        data=None
+    )
+    
         
 
