c2e8ee768fcea03d1f1ccf7100be6fe79e342f90
lovevyas <love1252.be21@chitkarauniversity.edu.in>
2026-06-10 17:45:48 +0530

    Task 3 routing completed

--- student_support_api/main.py
+++ student_support_api/main.py
@@ -1,9 +1,38 @@
 from fastapi import FastAPI
+from schemas.ticket import TicketCreate
+from schemas.response import APIResponse
+from schemas.ticket import TicketBase
 
 app = FastAPI(title="Student Support Ticket API")
+tickets_db = []
 
 @app.get("/")
 def home():
-    return {
-        "message": "Student Support Ticket API is running"
-    }
+    return APIResponse(
+        success = True,
+        message = "Student Support Ticket API is running",
+        data = None
+    )
+    
+@app.post(
+    "/ticket",
+    response_model = APIResponse
+)
+def create_ticket(ticket: TicketCreate):
+    tickets_db.append(ticket.model_dump())
+    return APIResponse(
+        success=True,
+        message="Ticket created succesfully",
+        data=ticket.model_dump()
+    )
+
+@app.get("/tickets",
+         response_model=APIResponse)
+def get_ticket():
+    return APIResponse(
+        success = True,
+        message = "All tickets are here",
+        data = tickets_db
+    )
+        
+
